5af8b7db9d94f76cab1e8a127de6f1c19a69417c
Hacking Notes <118412415+Hacking-Notes@users.noreply.github.com>
2023-03-07 18:58:37 -0500

    Update structure.py

--- modules/structure.py
+++ modules/structure.py
@@ -1,37 +1,67 @@
-from dataclasses import dataclass, field
-from dataclass_wizard import JSONWizard
+from marshmallow import Schema, fields
 
 
-@dataclass
-class PoC(JSONWizard):
-    source: str = "exploitdb"
-    title: str = "Exploit for something"
-    link: str = "https://example.com"
-    parent_name: str = "CVE-2000-0000"
+class PoCSchema(Schema):
+    source = fields.String()
+    title = fields.String()
+    link = fields.String()
+    parent_name = fields.String()
 
 
-@dataclass(order=True)
-class Vulnerability(JSONWizard):
-    name: str = "Default vulnerability"
-    description: str = "Default vulnerability description"
-    severity: str = "0.0"
-    pocs: list[PoC] = field(default_factory=list)
+class PoC:
+    def __init__(self, source="exploitdb", title="Exploit for something",
+                 link="https://example.com", parent_name="CVE-2000-0000"):
+        self.source = source
+        self.title = title
+        self.link = link
+        self.parent_name = parent_name
 
-    def __post_init__(self):
-        self.sort_index = float(self.severity)
+    def __repr__(self):
+        return f"PoC(source={self.source}, title={self.title}, link={self.link}, parent_name={self.parent_name})"
 
 
-@dataclass(order=True)
-class Technology(JSONWizard):
-    name: str = "Default technology"
-    version: str = "0.0.0"
-    confidence: int = 0
-    cpe_name: str = "cpe:2.3:*:*:*:*:*:*:*:*:*:*:*"
-    cpe_ID: str = ""
-    vulns: list[Vulnerability] = field(default_factory=list)
+class VulnerabilitySchema(Schema):
+    name = fields.String()
+    description = fields.String()
+    severity = fields.String()
+    pocs = fields.Nested(PoCSchema, many=True)
 
-    def __post_init__(self):
-        self.sort_index = self.confidence
+
+class Vulnerability:
+    def __init__(self, name="Default vulnerability",
+                 description="Default vulnerability description",
+                 severity="0.0", pocs=None):
+        self.name = name
+        self.description = description
+        self.severity = severity
+        self.pocs = pocs or []
+
+    def __repr__(self):
+        return f"Vulnerability(name={self.name}, description={self.description}, severity={self.severity}, pocs={self.pocs})"
+
+
+class TechnologySchema(Schema):
+    name = fields.String()
+    version = fields.String()
+    confidence = fields.Integer()
+    cpe_name = fields.String()
+    cpe_ID = fields.String()
+    vulns = fields.Nested(VulnerabilitySchema, many=True)
+
+
+class Technology:
+    def __init__(self, name="Default technology", version="0.0.0",
+                 confidence=0, cpe_name="cpe:2.3:*:*:*:*:*:*:*:*:*:*:*", cpe_ID="",
+                 vulns=None):
+        self.name = name
+        self.version = version
+        self.confidence = confidence
+        self.cpe_name = cpe_name
+        self.cpe_ID = cpe_ID
+        self.vulns = vulns or []
+
+    def __repr__(self):
+        return f"Technology(name={self.name}, version={self.version}, confidence={self.confidence}, cpe_name={self.cpe_name}, cpe_ID={self.cpe_ID}, vulns={self.vulns})"
 
 
 class Status:
